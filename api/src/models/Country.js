dfc82d73e1b9e50257c7cb0f5dfd3ed7705cf361
Nacho <ignaciopaez96@gmail.com>
2021-10-26 20:53:08 -0300

    model1

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,10 +3,37 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define('Countries', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    alpha3Code: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true
+    },
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    region: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.INTEGER,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+    }
+
   });
 };
