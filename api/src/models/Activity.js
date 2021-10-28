20c0e969833e39aa7fe140944175b65f423e63e0
Nacho <ignaciopaez96@gmail.com>
2021-10-28 14:51:51 -0300

    models4

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -1,29 +1,30 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-module.exports = (Sequelize) => {
-    // defino el modelo
-    Sequelize.define('Activities',{
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Activity",
+    {
+      name: {
+        type: DataTypes.STRING,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        validate: {
+          min: 1,
+          max: 5,
         },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        difficulty: {
-            type: DataTypes.ENUM(['1', '2', '3', '4', '5']),
-            allowNull: true,
-        },
-        duration: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        },
-        season: {
-            type: DataTypes.ENUM('["Winter", "Autumn", "Spring", "Summer"]'),
-            allowNull: true
-        },
-    });
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+      },
+      season: {
+        type: DataTypes.ENUM("summer", "fall", "winter", "spring"),
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 };
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,39 +1,51 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('Countries', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Country",
+    {
+      alpha3Code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+        validate: {
+          isAlpha: true,
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      flag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      region: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      area: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      demonyms: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-    alpha3Code: {
-      type: DataTypes.STRING(3),
-      allowNull: false,
-      primaryKey: true
-    },
-    flag: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    capital: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    region: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subregion: {
-      type: DataTypes.STRING,
-    },
-    area: {
-      type: DataTypes.INTEGER,
-    },
-    population: {
-      type: DataTypes.INTEGER,
+    {
+      timestamps: false,
     }
-
-  });
+  );
 };
