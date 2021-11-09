ce025e6d2d04fef6f11fc89965cd44558f1013c9
Nacho <ignaciopaez96@gmail.com>
2021-11-09 16:22:01 -0300

    testeanding2

--- api/tests/models/country.spec.js
+++ api/tests/models/country.spec.js
@@ -20,3 +20,23 @@ describe('Country model', () => {
     });
   });
 });
+
+// xdescribe('Activity model', () => {
+//   before(() => conn.authenticate()
+//     .catch((err) => {
+//       console.error('Unable to connect to the database:', err);
+//     }));
+//   describe('Validators', () => {
+//     beforeEach(() => Activity.sync({ force: true }));
+//     describe('name', () => {
+//       it('should throw an error if name is null', (done) => {
+//         Activity.create({})
+//           .then(() => done(new Error('It requires a valid name')))
+//           .catch(() => done());
+//       });
+//       it('should work when its a valid name', () => {
+//         Activity.create({ name: 'Eco Tourism' });
+//       });
+//     });
+//   });
+// });
